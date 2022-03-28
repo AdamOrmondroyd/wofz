4fe187e2f1fecdaf83bad4dd860f8a472b3442a4
Ormorod <Adam.Ormondroyd@gmail.com>
2022-03-28 15:24:57 +0100

    commented out camb.dark_energy idea

--- src/dark_linf.py
+++ src/dark_linf.py
@@ -1,7 +1,8 @@
-from ctypes import c_double
+# from ctypes import c_double
 
 import numpy as np
-from camb.dark_energy import DarkEnergyPPF
+
+# from camb.dark_energy import DarkEnergyPPF
 from cobaya import Theory
 from linf import AdaptiveLinf
 
@@ -9,47 +10,47 @@ a_min = 1 / 3
 a_today = 1
 
 
-class DarkEnergyLinf(DarkEnergyPPF):
+# class DarkEnergyLinf(DarkEnergyPPF):
 
-    params = {
-        "N": None,
-        "w0": None,
-        "a1": None,
-        "w1": None,
-        "a2": None,
-        "w2": None,
-        "w3": None,
-    }
+#     params = {
+#         "N": None,
+#         "w0": None,
+#         "a1": None,
+#         "w1": None,
+#         "a2": None,
+#         "w2": None,
+#         "w3": None,
+#     }
 
-    # what the heck are we actually meant to do here? I don't feel like this
-    # can possibly be the way given how they went on about putting so much
-    # effort into writing the python wrapper. Why does it lack clear instructions?
-    # I think the only clear reason for this can be that I'm barking up the wrong tree.
-    # someone must have tried a different model for dark energy at some point
+#     # what the heck are we actually meant to do here? I don't feel like this
+#     # can possibly be the way given how they went on about putting so much
+#     # effort into writing the python wrapper. Why does it lack clear instructions?
+#     # I think the only clear reason for this can be that I'm barking up the wrong tree.
+#     # someone must have tried a different model for dark energy at some point
 
-    def __init__(self, *args, **kwargs):
-        print("DarkEnergyLinf constructor")
-        return super().__init__(*args, **kwargs)
+#     def __init__(self, *args, **kwargs):
+#         print("DarkEnergyLinf constructor")
+#         return super().__init__(*args, **kwargs)
 
-    def set_params(self, N, w0, a1, w1, a2, w2, w3):
-        print("setting DarkEnergyLinf params")
-        self.N = N
-        self.w0 = w0
-        self.a1 = a1
-        self.w1 = w1
-        self.a2 = a2
-        self.w2 = w2
-        self.w3 = w3
+#     def set_params(self, N, w0, a1, w1, a2, w2, w3):
+#         print("setting DarkEnergyLinf params")
+#         self.N = N
+#         self.w0 = w0
+#         self.a1 = a1
+#         self.w1 = w1
+#         self.a2 = a2
+#         self.w2 = w2
+#         self.w3 = w3
 
-        self.linf = AdaptiveLinf(a_min, a_today)
-        theta = np.array([N, w0, a1, w1, a2, w2, w3])
+#         self.linf = AdaptiveLinf(a_min, a_today)
+#         theta = np.array([N, w0, a1, w1, a2, w2, w3])
 
-        # use linspace to fill w(a) so that cubic spline becomes linf
-        a = np.linspace(a_min, a_today)  # linspace
-        w = self.linf(a, theta)
+#         # use linspace to fill w(a) so that cubic spline approximates linf
+#         a = np.linspace(a_min, a_today)  # linspace
+#         w = self.linf(a, theta)
 
-        self.use_tabulated_w = True
-        self.set_w_a_table(a, w)
+#         self.use_tabulated_w = True
+#         self.set_w_a_table(a, w)
 
 
 class DarkEnergyLinfTheory(Theory):
@@ -83,7 +84,8 @@ class DarkEnergyLinfTheory(Theory):
         print(theta)
 
         # use linspace to fill w(a) so that cubic spline becomes linf (might want to increase number of points)
-        a = np.linspace(a_min, a_today)  # linspace
+        # a = np.linspace(a_min, a_today)  # linspace
+        a = np.array([a_min, a1, a2, a_today])
         w = linf(a, theta)
         state["dark_energy"] = {
             "a": a,
@@ -93,5 +95,5 @@ class DarkEnergyLinfTheory(Theory):
     def get_dark_energy(self):
         print("getting DarkEnergyLinf")
         result = self.current_state["dark_energy"]
-        print("result found")
+        print(f"result found:{result}")
         return result
